1cdb0577737a7b31a3ea8b83cabd15c7ff421aec
kazu5150 <kazu.deadtotheworld@gmail.com>
2024-08-18 07:30:56 +0900

    spreadsheet & yamato0819

--- kuroneko_yamato.py
+++ kuroneko_yamato.py
@@ -1,40 +1,22 @@
-# 仮装環境で動きます。 source selenium_env/bin/activate
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# 問い合わせ番号を入力して下さい。
-contact_number = input("問い合わせ番号を入力して下さい。")
 
-# WebDriverの初期化（ChromeDriverの場合）
-driver = webdriver.Chrome()
-
-# お問い合わせページにアクセス
-driver.get("https://toi.kuronekoyamato.co.jp/cgi-bin/tneko")
-
-# ページソースを確認
-# print(driver.page_source)
-
-# 問い合わせ番号を入力
-input_field = driver.find_element(By.NAME, "number01")
-input_field.send_keys(contact_number)  # 画像の番号を使用
-
-# お問い合わせ開始ボタンをクリック
-submit_button = driver.find_element(
-    By.CSS_SELECTOR, "button[type='submit'][name='category'][value='0']")
-submit_button.click()
-
-# 結果が表示されるまで待機
-wait = WebDriverWait(driver, 10)
-result_element = wait.until(
-    EC.presence_of_element_located((By.CLASS_NAME, "js-tracking-detail")))
-
-# テキストを取得
-result_text = result_element.text
-
-print(f"【{contact_number}】のお荷物：【{result_text}】")  # "伝票番号未登録" が表示されるはず
-
-# ブラウザを閉じる
-driver.quit()
+# 問い合わせ番号を渡されると、seleniumが起動し、荷物の状況を取得し返します。
+def check_yamato_delivery(contact_number):
+    driver = webdriver.Chrome()
+    try:
+        driver.get("https://toi.kuronekoyamato.co.jp/cgi-bin/tneko")
+        input_field = driver.find_element(By.NAME, "number01")
+        input_field.send_keys(contact_number)
+        submit_button = driver.find_element(
+            By.CSS_SELECTOR, "button[type='submit'][name='category'][value='0']")
+        submit_button.click()
+        wait = WebDriverWait(driver, 10)
+        result_element = wait.until(
+            EC.presence_of_element_located((By.CLASS_NAME, "js-tracking-detail")))
+        return result_element.text
+    finally:
+        driver.quit()
--- yamato_spread.py
+++ yamato_spread.py
@@ -1,6 +1,15 @@
+"""
+この関数を実行すると、spreadsheetから問い合わせ番号を取得し、kuroneko_yamato.pyにてseleniumを実行し、
+結果を受け取ります。
+"""
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+import time
+from datetime import datetime
+
+from kuroneko_yamato import check_yamato_delivery
 
 
 SCOPE = [
@@ -19,19 +28,34 @@ worksheet = workbook.worksheet("発送管理")
 # print(workbook.title)
 # print(workbook.id)
 
-# セルの値を取得するには acell() メソッドを使う
-shipping_name = worksheet.acell("B2").value
-contact_number = worksheet.acell("C2").value
-
-# print(f"発送先：【{shipping_name}】  問い合わせ番号：【{contact_number}】")
-
 df = pd.DataFrame(worksheet.get_all_values())
 df.columns = df.iloc[0]
 df = df.drop(df.index[0])
 
-
-print(df["問い合わせ番号"])
-
-
 contact_numbers = df["問い合わせ番号"].tolist()
 print(contact_numbers)
+
+# contact_number = contact_numbers[1]
+
+# result = check_yamato_delivery(contact_number)
+# print(f"【{contact_number}】のお荷物：【{result}】")
+
+results = []
+for contact_number in contact_numbers:
+    result = check_yamato_delivery(contact_number)
+    print(f"【{contact_number}】のお荷物：【{result}】")
+    results.append(result)
+    time.sleep(3)
+
+print(results)
+worksheet.update([[result] for result in results], 'D2:D8')
+
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(current_time)
+worksheet.update_cell(2, 5, current_time)
+worksheet.update_cell(3, 5, current_time)
+worksheet.update_cell(4, 5, current_time)
+worksheet.update_cell(5, 5, current_time)
+worksheet.update_cell(6, 5, current_time)
+worksheet.update_cell(7, 5, current_time)
+worksheet.update_cell(8, 5, current_time)
